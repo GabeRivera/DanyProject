18a175d0b6b4eb14f787ff512132facde7677f6a
Gabe Rivera <gabriel.f.rivera.10@gmail.com>
2020-05-11 13:44:00 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#This project is to help you learn a few things
+
+I'll assign problems to you. You will solve them and submit them for my review and advice. 
+
+I will add a file with a problem name to the Problems directory. You will try to solve the problem. 
+Once you are ready you will submit a Pull Request with your solution. 
+
+I will then review and give you feedback. 
---
+++ problems/fizzbuzz.js
@@ -0,0 +1,11 @@
+// This is a classic programing problem.
+// It's a great starting point
+
+// PROBLEM:
+// Write a program that console.log()s
+// Every number from 1 - 100
+// If a number is divisible by 3 console.log('Fizz')
+// If the number is divisible by 5 console.log('Buzz')
+// If the number is divisible by both, console.log('FizzBuzz');
+// Example: 1, 2, FIZZ, 4, BUZZ... 14, FIZZBUZZ
+
