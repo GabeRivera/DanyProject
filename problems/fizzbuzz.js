fbff74d9d76f09b0821483f368b7d8fb5f4bc0e9
Carlos Daniel Duque Rivera <Carlos_Duque@Rafaels-MacBook-Air.local>
2020-06-01 18:08:03 -0400

    This is a test commit

--- problems/fizzbuzz.js
+++ problems/fizzbuzz.js
@@ -16,13 +16,12 @@
 // DRY : Don't Repeat Yourself
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        console.log('Fizz');
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
     } else if (i % 5 === 0) {
-        console.log('Buzz')
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz')
-    } else {
-        console.log(i);
+      console.log('Buzz');
     }
-}
+   else console.log(i);
+  }
